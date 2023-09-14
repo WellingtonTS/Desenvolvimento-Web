f1ba4b03461b91bd37656ee40aa67f8cd51021c1
WellingtonTS <torquatojucas@gmail.com>
2023-09-14 20:36:56 -0300

    add course

---
+++ 4-JavaScript/class-01-data-types/script.js
@@ -0,0 +1,28 @@
+// Primitives (String, Number, Boolean...)
+
+let name = "Wellington";
+let age = 45
+let weight = 69;
+
+console.log(name);
+console.log(45);
+console.log(age);
+console.log(typeof weight);
+
+let number1 = 2;
+let number2 = 5;
+
+console.log(number1 + number2);
+
+let isStudent = true;
+
+console.log(isStudent);
+console.log(10 == 10);
+console.log(10 == 20);
+console.log(10 > 9);
+console.log(10 < 9);
+console.log(10 != 9);
+
+let yourName = prompt("Digite seu nome: ");
+
+console.log(yourName);
