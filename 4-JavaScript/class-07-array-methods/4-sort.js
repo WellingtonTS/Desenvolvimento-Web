adbe81d8b7f8fb608b3198ab8c0d1906ee9d4411
WellingtonTS <torquatojucas@gmail.com>
2023-10-23 20:38:43 -0300

    add sort exemple

---
+++ 4-JavaScript/class-07-array-methods/4-sort.js
@@ -0,0 +1,44 @@
+const arrayNumbers = [7, 4, 5, 7, 8, 9, "7", 40];
+
+// const value = arrayNumbers.sort((a, b) => {
+//     if (a > b) return 1;
+//     if (a < b) return -1;
+//     return 0;
+// });
+
+const arraySorted = arrayNumbers.sort((a, b) => a - b);
+
+console.log(arraySorted.join("-"));
+console.log(arrayNumbers.join("-"));
+
+const arrayNames = ["Pedrinho", "eduardo", "Luiza"];
+
+arrayNames.sort((a, b) => a - b);
+
+console.log(arrayNames);
+
+
+const arrayNames2 = ["Pedrinho", "eduardo", "Luiza"];
+arrayNames2.sort((a, b) => {
+    a = a.toUpperCase();
+    b = b.toUpperCase();
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+});
+
+console.log(arrayNames2);
+
+
+const arrayNames3 = ["Pedrinho", "eduardo", "Luiza", "Éric"];
+
+arrayNames3.sort((a, b) => {
+    a = a.toUpperCase();
+    b = b.toUpperCase();
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+});
+arrayNames3.sort((a, b) => a.localeCompare(b))
+
+console.log(arrayNames3);
