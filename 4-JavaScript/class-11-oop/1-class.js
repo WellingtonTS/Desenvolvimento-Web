06a1ea0431cda1e4dec997b0ae54d1bc9e53d177
WellingtonTS <torquatojucas@gmail.com>
2023-11-17 20:33:58 -0300

    add class 11 inherit

--- 4-JavaScript/class-11-oop/1-class.js
+++ 4-JavaScript/class-11-oop/1-class.js
@@ -27,3 +27,41 @@ console.log(redBrush.height);
 
 console.log(blueBrush.colorName, blueBrush.height);
 console.log(redBrush.colorName, redBrush.height);
+
+class Pokemon {
+    constructor(name, level) {
+    this.name = name;
+    this.level = level;
+    this.health = 100;
+    }
+
+    attack() {
+    console.log(`${this.name} atacou!`);
+    }
+
+    damage(effective, damage = 20){
+    if (effective) {
+        this.health -= damage * 1.3;
+    } else {
+        this.health -= damage;
+    }
+    }
+}
+
+const charmander = new Pokemon("charmander", 5);
+const squirtle = new Pokemon("squirtle", 4);
+const bulbasaur = new Pokemon("bulbasaur", 3);
+
+console.log(charmander.name);
+console.log(charmander.level);
+console.log(squirtle.name);
+
+squirtle.attack();
+
+console.log(bulbasaur.health);
+bulbasaur.damage();
+console.log(bulbasaur.name, bulbasaur.health);
+
+const effective = true
+bulbasaur.damage(effective);
+console.log(bulbasaur.name, bulbasaur.health);
