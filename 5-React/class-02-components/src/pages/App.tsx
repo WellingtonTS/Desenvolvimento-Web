36015d32771cec5ca36bdfa9a5df08d251bd74bd
WellingtonTS <torquatojucas@gmail.com>
2023-12-06 20:34:05 -0300

    add component card profile

---
+++ 5-React/class-02-components/src/pages/App.tsx
@@ -0,0 +1,10 @@
+import { CardProfile } from "../components/CardProfile";
+
+export function App() {
+  return (
+    <>
+      <h1>Class 02 Components</h1>
+      <CardProfile name={"wellington"} age={45}/>
+    </>
+  );
+}
