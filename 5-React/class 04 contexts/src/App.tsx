44a0a633dde7f96f9ff4af7e5aadd433954e50cc
WellingtonTS <torquatojucas@gmail.com>
2023-12-12 20:42:51 -0300

    add class contexts

---
+++ 5-React/class 04 contexts/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Button } from "./components/Button";
+import { UserCard } from "./components/UserCard";
+import { UserContext } from "./contexts/UserContext";
+
+export function App() {
+  const [name, setName] = useState("Wellington");
+
+  return (
+    <UserContext.Provider value={{ name, setName }}>
+      <h1>Class 04 Contests</h1>
+      <UserCard />
+      <Button />
+    </UserContext.Provider>
+  );
+}
