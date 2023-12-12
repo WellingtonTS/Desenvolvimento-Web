b1a74cc1a762689026812e95a429d9b5031beb73
WellingtonTS <torquatojucas@gmail.com>
2023-12-12 19:36:49 -0300

    refactor user card

--- 5-React/class-03-states/src/App.tsx
+++ 5-React/class-03-states/src/App.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { UserCard } from "./components/UserCard";
 
 type UserData = {
   name: string;
@@ -41,17 +42,16 @@ export function App() {
       <p>Count: {count}</p>
       <button onClick={addCount}>Add&gt;</button>
 
-      <form onSubmit={handleSubmit} style={{ textAlign: "center", marginTop: 24 }}>
+      <form
+        onSubmit={handleSubmit}
+        style={{ textAlign: "center", marginTop: 24 }}
+      >
         <input type="text" onChange={handleChangeName} />
         <button>Pesquisar</button>
       </form>
 
       {data && (
-        <section style={{ textAlign: "center" }}>
-          <h2>{data.name}</h2>
-          <img src={data.avatar_url} alt="" width={240} />
-          <p>{data.bio}</p>
-        </section>
+        <UserCard name={data.name} avatar_url={data.avatar_url} bio={data.bio} />
       )}
     </>
   );
