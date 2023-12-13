90f3a4ad9d64eb72892733301c6e67c730957fa5
WellingtonTS <torquatojucas@gmail.com>
2023-12-13 20:53:39 -0300

    add class 05  CSS in JS

---
+++ 5-React/class-05-css-in-js/src/components/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+background: ${({ theme }) => theme.colors.backgroubdButton};
+height: 56px;
+width: 219px;
+font-weight: bold;
+`;
---
+++ 5-React/class-05-css-in-js/src/pages/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../components/Button";
+import { AppContanier } from "./styles";
+
+export function App() {
+  return (
+    <AppContanier>
+      <h1>Class CSS in JS</h1>
+      <Button />
+    </AppContanier>
+  );
+}
