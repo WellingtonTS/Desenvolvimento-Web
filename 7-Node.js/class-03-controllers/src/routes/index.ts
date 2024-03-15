b9bc72259a2d1ddaedfdac273a1640787bc174ce
Wellington <torquatojucas@gmail.com>
2024-03-15 20:19:59 -0300

    add controllers update and delete

--- 7-Node.js/class-03-controllers/src/routes/index.ts
+++ 7-Node.js/class-03-controllers/src/routes/index.ts
@@ -5,3 +5,5 @@ export const router = Router();
 
 router.post("/user", userControllers.create);
 router.get("/user/:id", userControllers.read);
+router.put("/user/:id", userControllers.update);
+router.delete("/user/:id", userControllers.delete);
